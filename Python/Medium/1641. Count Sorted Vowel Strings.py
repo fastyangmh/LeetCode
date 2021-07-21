12090d8603c93b3a1716c39af0225a999a1cd9a0
YangMH <fastyangmh@gmail.com>
2021-07-21 22:38:57 +0800

    add other solution.

--- Python/Medium/1641. Count Sorted Vowel Strings.py
+++ Python/Medium/1641. Count Sorted Vowel Strings.py
@@ -13,6 +13,25 @@ class Solution:
             last_sum = sum(array)
         return last_sum
 
+    ''' other solution, this is funny
+    class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        # a, e, i, o, u
+        transformation_matrix = [[1, 1, 1, 1, 1],
+                                 [0, 1, 1, 1, 1],
+                                 [0, 0, 1, 1, 1],
+                                 [0, 0, 0, 1, 1],
+                                 [0, 0, 0, 0, 1]]
+        v_counts = [1]*5
+        for _ in range(n-1):
+            v_counts = [self.dot(v_counts, row)
+                        for row in transformation_matrix]
+        return sum(v_counts)
+
+    def dot(self, l1, l2):
+        return sum([x*y for x, y in zip(l1, l2) if y != 0])
+    '''
+
 
 if __name__ == '__main__':
     # ex1    ans 5
