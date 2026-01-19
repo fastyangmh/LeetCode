cdeffc1e70523731d1514d8bbd62373a083d2808
Austin Yang <fastyangmh@gmail.com>
2026-01-19 20:16:12 +0800

    add some solutions

--- Python/2. Add Two Numbers.py
+++ Python/2. Add Two Numbers.py
@@ -11,15 +11,15 @@ class Solution:
         carry = 0
 
         while l1 or l2 or carry:
-            n1 = l1.val if l1 else 0
-            n2 = l2.val if l2 else 0
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-            val = n1 + n2 + carry
-            carry = 1 if val >= 10 else 0
-            val = val % 10
+            val = (v1 + v2 + carry) % 10
+            carry = (v1 + v2 + carry) // 10
 
             node.next = ListNode(val)
             node = node.next
+
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
